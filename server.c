74faa40d63f1209660648da317a76d24db18e0ff
Pedro Baptista <ze.pedro3622@outlook.pt>
2019-05-09 19:38:27 +0100

    Basic argument handler added to server program

--- server.c
+++ server.c
@@ -1,7 +1,38 @@
 #include "sope.h"
+#include <stdlib.h>
+
+int checkArgs(const int argc, const char *argv[])
+{
+    if(argc != 3)
+    {
+        printf("Usage: ./server <nBankOffices> \"<passwordAdmin>\"\n");
+        return 1;
+    }
+
+    int nBankOffices = atoi(argv[1]);
+    if(nBankOffices <= 0)
+    {
+        printf("Invalid nBanckOffices\n");
+        return 1;
+    }
+
+    if(nBankOffices > MAX_BANK_OFFICES)
+    {
+        printf("Too many bank offices!\n");
+        return 1;
+    }
+
+    return 0;
+}
+
 
 int main (int argc, char *argv[], char *envp[])
 {
+    if(checkArgs(argc, argv)) //This func returns 1 if there's any problem with the arguments
+        return 1;             //If that happens, the program closes.
+
     printf("Server running!\n");
+
+    printf("Server's dead.\n");
     return 0;
 }
